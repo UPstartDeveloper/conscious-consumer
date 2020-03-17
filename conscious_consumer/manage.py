fdfb13bb08519f70903add392c5fd0b9e7df35e6
= <zainr7989@gmail.com>
2020-03-17 09:48:38 -0700

    Add base template.

--- conscious_consumer/manage.py
+++ conscious_consumer/manage.py
@@ -5,7 +5,8 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conscious_consumer.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                          'conscious_consumer.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
