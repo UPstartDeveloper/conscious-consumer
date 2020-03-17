7746de502da8e89dc06ec7fdf6f5666ee6ddf7c1
= <zainr7989@gmail.com>
2020-03-17 15:06:32 -0700

    Add signup view controller.

--- conscious_consumer/accounts/urls.py
+++ conscious_consumer/accounts/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 import django.contrib.auth.views as auth_views
+from .views import (
+    SignupView,
+)
 
 app_name = 'accounts'
 urlpatterns = [
+    # auth related views
     path('login/', auth_views.LoginView.as_view(
             template_name='accounts/auth/login.html'), name="login"),
+    path('signup/', SignupView.as_view(), name='signup'),
+    # profile related views
 ]
--- conscious_consumer/accounts/views.py
+++ conscious_consumer/accounts/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.conf import settings as dj_conf_settings
+from django.views.generic.edit import (
+    CreateView,
+    UpdateView,
+    DeleteView)
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from .models import Profile
 
-# Create your views here.
+
+class SignupView(SuccessMessageMixin, CreateView):
+    '''User is able to signup with a username, email, and password.'''
+    # form_class
+    success_url = reverse_lazy('accounts:login')
+    template_name = 'accounts/signup.html'
+    success_message = "Congratulations! You may now log in."
+
+    def form_valid(self, form):
+        '''Save the new User, and set up their profile as well.'''
+        self.object = form.save()
+        profile = Profile.objects.create(user=self.object)
+        profile.save()
+        return super().form_valid(form)
