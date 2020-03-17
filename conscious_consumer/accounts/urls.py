27886d11272929b6f0586be230dae742a31fb29c
= <zainr7989@gmail.com>
2020-03-17 14:42:56 -0700

    Add login view controller and init template.

--- conscious_consumer/accounts/urls.py
+++ conscious_consumer/accounts/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
+import django.contrib.auth.views as auth_views
 
 app_name = 'accounts'
-urlpatterns = []
+urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(
+            template_name='accounts/auth/login.html'), name="login"),
+]
