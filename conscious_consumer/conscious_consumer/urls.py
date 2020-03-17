e41561f405a9b3bafc31e408a75c18747cb9fd2f
= <zainr7989@gmail.com>
2020-03-17 13:12:46 -0700

    Include app urls in project.

--- conscious_consumer/conscious_consumer/urls.py
+++ conscious_consumer/conscious_consumer/urls.py
@@ -14,10 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .views import landing
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Project-wide urls
     path('', landing, name="landing_page"),
+    # Auth-related urls
+    path('accounts/', include('accounts.urls')),
+    # Carbon-budgeting urls
+    path('budget/', include('budget.urls')),
+    # E-commerce-related urls
+    path('store/', include('store.urls')),
 ]
