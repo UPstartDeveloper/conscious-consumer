fdff8621d9d67d226de7067d9e99180ce3309183
= <zainr7989@gmail.com>
2020-03-17 17:09:48 -0700

    Add forms based on Goal and Comment models.

---
+++ conscious_consumer/budget/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from .models import Goal, Comment
+
+
+class GoalForm(forms.ModelForm):
+    class Meta:
+        model = Goal
+        exclude = [
+            'author',
+            'slug',
+            'created',
+            'modified',
+        ]
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = [
+            'headline',
+            'description',
+        ]
