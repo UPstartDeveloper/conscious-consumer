024e6575a19005e3c4fb3d37c7576579e07b1bfe
= <zainr7989@gmail.com>
2020-03-18 18:24:13 -0700

    Update Product and add Review models.

--- conscious_consumer/store/admin.py
+++ conscious_consumer/store/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Review
 from conscious_consumer.settings import ADMIN_TITLE
 
 admin.site.site_header = ADMIN_TITLE
-admin.site.register(Product)
+admin.site.register(Product, Review)
