a14506ce0d999ae3be31b22b72c706dc9b27c4fe
= <zainr7989@gmail.com>
2020-03-18 19:15:58 -0700

    Add form class for Product and Review.

---
+++ conscious_consumer/store/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from .models import Product, Review
+
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = [
+            'title',
+            'description',
+            'photo',
+            'price'
+        ]
+
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        exclude = [
+            'author',
+            'modified'
+        ]
