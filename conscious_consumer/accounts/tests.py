745040f8529bf1729df3571fb974e241acbf4c8e
= <zainr7989@gmail.com>
2020-03-22 16:42:17 -0700

    Test site visitor accesses signup form.

--- conscious_consumer/accounts/tests.py
+++ conscious_consumer/accounts/tests.py
@@ -26,25 +26,19 @@ class SignupViewTests(TestCase):
         )
         self.url = 'accounts:signup'
 
-    def test_get_signup_form(self):
+    def test_visitor_gets_signup_form(self):
         '''Site visitor is able to see the form to sign up for an account.'''
-        pass
+        # a site visitor (not logged in) makes a request and gets a response
+        self.assertTrue(self.visitor.is_anonymous)
+        request = self.factory.get(self.url)
+        request.user = self.visitor
+        response = SignupView.as_view()(request)
+        # response is valid
+        self.assertEqual(response.status_code, 200)
+        # response renders the form
+        self.assertContains(response, 'Join Now, and Set Your Carbon Budget!')
 
-    def test_form_valid_submission(self):
-        """
-        The visitor enters valid data, the User and Profile objects are made,
-        and then the site visitor is redirected to the login page.
-        """
-        pass
-
-    def test_form_password_no_match(self):
-        """
-        The site visitor fails to confirm their password, and the form renders
-        itself again with an error message.
-        """
-        pass
-
-    def test_authenticated_user_accesses_form(self):
+    def test_authenticated_user_gets_signup_form(self):
         """
         An authenticated user is no longer able to fill out the signup form.
         """
@@ -60,6 +54,20 @@ class SignupViewTests(TestCase):
         self.assertContains(response,
                             "Looks like you already have an account. " +
                             "We appreciate you for joining us!")
+
+    def test_form_valid_submission(self):
+        """
+        The visitor enters valid data, the User and Profile objects are made,
+        and then the site visitor is redirected to the login page.
+        """
+        pass
+
+    def test_form_password_no_match(self):
+        """
+        The site visitor fails to confirm their password, and the form renders
+        itself again with an error message.
+        """
+        pass
 
 
 class ProfileDetailTests(TestCase):
