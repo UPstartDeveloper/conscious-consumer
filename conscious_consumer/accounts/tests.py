7f65ced2679ae045e8bf14bb5e8419abb865e1c9
= <zainr7989@gmail.com>
2020-03-22 16:05:57 -0700

    Declare additional accounts tests.

--- conscious_consumer/accounts/tests.py
+++ conscious_consumer/accounts/tests.py
@@ -11,7 +11,24 @@ from .views import (
 class SignupViewTests(TestCase):
     '''User signup for an account and also receive a profile in the db.'''
     def setUp(self):
-        '''Site visitor information relevant  to each test.'''
+        '''Site visitor information relevant to each test.'''
+
+    def test_get_signup_form(self):
+        '''Site visitor is able to see the form to sign up for an account.'''
+        pass
+
+    def test_form_valid_submission(self):
+        """
+        The visitor enters valid data, the User and Profile objects are made,
+        and then the site visitor is redirected to the login page.
+        """
+        pass
+
+    def test_form_password_no_match(self):
+        """
+        The site visitor fails to confirm their password, and the form renders
+        itself again with an error message.
+        """
         pass
 
 
