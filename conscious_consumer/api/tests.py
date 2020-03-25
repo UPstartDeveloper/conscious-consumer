6d46aebaaae6dc0b668c41401e5bb1772a1df9ab
= <zainr7989@gmail.com>
2020-03-24 18:32:52 -0700

    Declare test cases for api.

--- conscious_consumer/api/tests.py
+++ conscious_consumer/api/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.test.client import RequestFactory
+from .serializers import GoalSerializer
+from .views import GoalData
 
-# Create your tests here.
+
+class GoalDataTests(TestCase):
+    def setUp(self):
+        '''User has already inserted a Goal into the db.'''
+        pass
+
+    def test_retrieve_achievement_failure_data():
+        '''The view returns achievement and failure data for a given Goal.'''
+        pass
