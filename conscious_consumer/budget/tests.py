a010be614a4f40cf73ea240ce5a5103cacf6ce2c
= <zainr7989@gmail.com>
2020-03-25 10:08:02 -0700

    Declare budget TestCases.

--- conscious_consumer/budget/tests.py
+++ conscious_consumer/budget/tests.py
@@ -1,3 +1,39 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.test.client import RequestFactory
+from .views import (
+    AllGoalList,
+    PersonalGoalList,
+    PersonalGoalDetail,
+    PublicGoalDetail,
+    GoalCreate,
+    GoalUpdate,
+    GoalDelete,
+)
 
-# Create your tests here.
+
+class AllGoalListTests(TestCase):
+    pass
+
+
+class PersonalGoalListTests(TestCase):
+    pass
+
+
+class PersonalGoalDetailTests(TestCase):
+    pass
+
+
+class PublicGoalDetailTests(TestCase):
+    pass
+
+
+class GoalCreateTests(TestCase):
+    pass
+
+
+class GoalUpdateTests(TestCase):
+    pass
+
+
+class GoalDeleteTests(TestCase):
+    pass
