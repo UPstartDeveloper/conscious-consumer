84425080de71afd1983769562e406ec1e50924cc
= <zainr7989@gmail.com>
2020-03-25 10:16:38 -0700

    Implement AllGoalListTests.setUp.

--- conscious_consumer/budget/tests.py
+++ conscious_consumer/budget/tests.py
@@ -9,10 +9,28 @@ from .views import (
     GoalUpdate,
     GoalDelete,
 )
+from django.contrib.auth.models import User
+from .models import Goal
 
 
 class AllGoalListTests(TestCase):
-    pass
+    def setUp(self):
+        '''Instaniate relevant model instances reused in tests.'''
+        self.user = (
+            User.objects.create_user('zainraza',
+                                     'zainr7989@gmail.com',
+                                     'who_is_typing_this_9')
+        )
+        self.goal = Goal.objects.create(
+            title='Daily Commute',
+            author=self.user,
+            description='Make driving greener',
+            achievements=12,
+            fails=6,
+            category=Goal.TRAVEL_CAT,
+            monthly_target=Goal.MIN_VALUE
+        )
+        self.url = 'budget:goal_list_public'
 
 
 class PersonalGoalListTests(TestCase):
